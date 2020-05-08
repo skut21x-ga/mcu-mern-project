6a5fb11d02f6a6dbe373fcc9757ab17c37edf2de
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-08 02:53:58 -0400

    create character working. update needed

--- src/api.js
+++ src/api.js
@@ -4,7 +4,12 @@ export function createCharacter(data) {
   axios
     .post(
       `https://scott-mcu-api.herokuapp.com/`,
-      { Name: data.real_name, Species: data.species, Alias: data.alias },
+      {
+        real_name: data.real_name,
+        alias: data.alias,
+        species: data.species,
+        affiliation: data.affiliation,
+      },
       {
         headers: {
           "Access-Control-Allow-Origin": "scott-mcu-api.herokuapp.com/",
@@ -14,7 +19,7 @@ export function createCharacter(data) {
     .then((res) => {
       console.log(res);
     });
-  alert("New Character Created in Database");
+  // alert("New Character Created in Database");
 }
 
 export function updateCharacter(real_name, data) {
