133c7763a183ba1e26adf9d7a4472acb600e3cbd
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-08 02:03:10 -0400

    fixed yarn

--- src/api.js
+++ src/api.js
@@ -16,3 +16,25 @@ export function createCharacter(data) {
     });
   alert("New Character Created in Database");
 }
+
+export function updateCharacter(real_name, data) {
+  if (data.length >= 1) {
+    axios
+      .put(
+        `https://cors-anywhere.herokuapp.com/https://scott-mcu-api.herokuapp.com/name/${real_name}`,
+        { Character: data },
+        {
+          headers: {
+            "Access-Control-Allow-Origin":
+              "dakom1-crud-api.herokuapp.com/lists",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      });
+    window.setTimeout(function () {
+      window.location.reload(false);
+    }, 500);
+  }
+}
